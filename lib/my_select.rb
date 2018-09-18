bee44adb8a7aa81d2b0d49eab442edfe07910abb
Carson Rey <c.rey494@gmail.com>
2018-09-18 19:39:04 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,9 +4,11 @@ def my_select(collection)
  i = 0 
  while i < collection.length 
   if yield(collection[i])
-    storage <<  yield(collection[i])
+    storage << (collection[i])
+   i += 1
    end
-    i += 1 
  end
   storage
 end
+
+# .select is being passed into the yield block for this test, so yield(collection[i]) will evaluate to true. This opens up the method for a conditional. So if yield(collection[i]) is actually if true. Inside that conditional statement we store the actual part/iteration of the collection in 'storage'. Increment by 1 goes outside of the conditional block because it is part of the while loop

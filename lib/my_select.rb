c658d514c42afc276afa63764a2b9fb3c36d770c
Carson Rey <c.rey494@gmail.com>
2018-09-18 19:26:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
  storage = []
  i = 0 
  while i < collection.length 
-  if yield(collection[i]) == true 
+  if yield(collection[i])
     storage <<  yield(collection[i])
    end
     i += 1 
